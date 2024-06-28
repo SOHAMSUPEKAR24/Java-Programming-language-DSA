39ae6a0990ee5c0c688c5696ad19f909b2aa9759
SOHAMSUPEKAR24 <sohamsupekar24@gmail.com>
2024-06-28 11:38:41 +0530

    add files

--- JavaBasics.java
+++
@@ -1,9 +0,0 @@
-public class JavaBasics{
-    public static void main(String[] args) {
-        
-        int a = 30 ;
-        String type = ((a%2) == 0) ? "even" : "odd";
-        System.out.print(type);
-
-    }
-}
