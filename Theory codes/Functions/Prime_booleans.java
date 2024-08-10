17c634e2ab27fb9565eead6f2fc35d788d7288df
SOHAMSUPEKAR24 <sohamsupekar24@gmail.com>
2024-08-10 23:30:53 +0530

    fuhnction overloading

---
+++ Theory codes/Functions/Prime_booleans.java
@@ -0,0 +1,18 @@
+
+
+
+public class Prime_booleans{
+    public static boolean isprime(int n){
+        boolean isprime = true;
+        for(int i = 2 ; i <= n-1; i ++){
+            if( n%i == 0){
+                isprime = false;
+            }
+        }
+        return isprime;
+    }
+    public static void main (String args[]){
+        System.out.println(isprime(12));
+
+    }
+}
